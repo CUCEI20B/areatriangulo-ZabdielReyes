2ee6403c099f61e446934c7e8d6f052fc8deabfe
Zabdiel Reyes <alonso.salazar5525@alumnos.udg.mx>
2020-09-28 16:40:36 -0500

    prueba01

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main() {
     cout << "Hola Mundo!" << endl; // eliminar esta línea
-
+	unsigned int base;
+	unsigned int altura;
+	
+	cin>> base;
+	cin>> altura;
+	
+	cout <<(base * altura)/2 <<endl;
+	
+	
+	
     return 0;
 }
